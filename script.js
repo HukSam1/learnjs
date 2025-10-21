fb4602f98d695c9cdd1f58e54621e46704591438
Hugo Samuel Rohusaar <hugsamuelrohusaar@gmail.com>
2025-10-21 09:56:55 +0300

    Add array loop examples

---
+++ script.js
@@ -0,0 +1,33 @@
+let nums = [1, 2, 3, 4];
+let moreNums = [5, 6, ...nums, 7, 8];
+console.log(moreNums);
+
+function sum(...args) {
+    console.log(args);
+    let sum = 0;
+    for(let i=0; i<args.length; i++) {
+        sum+=args[i];
+    }
+    return sum;
+}
+
+sum(1,2);
+let a = sum(1,2,3,4,5,6,7,8,9);
+console.log(a);
+a = sum(...moreNums);
+console.log(a);
+
+let person = {
+    name: 'Hugo',
+    age: 17,
+}
+
+let {name, age} = person;
+console.log(name);
+console.log(age);
+
+function ageSquared({age}){
+    return age*age;
+}
+
+console.log (ageSquared(person));
